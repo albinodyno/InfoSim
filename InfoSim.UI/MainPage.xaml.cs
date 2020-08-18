4c9bb90148cb745b913aeb71be9680b839beb193
Jared Buchanan <jbbuchanan266@gmail.com>
2020-08-17 22:54:50 -0700

    new UI

---
+++ InfoSim.UI/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InfoSim.UI
+{
+    public sealed partial class MainPage : Page
+    {
+        int ticks = 0;
+        int firstCheck = 0;
+        int firstInt = 900;
+        int secondCheck = 0;
+        int secondInt = 1900;
+
+        bool sqlOnline = false;
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += TimerTick;
+            CheckAllSubs();
+            timer.Start();
+        }
+
+        public void CheckAllSubs()
+        {
+            sqlOnline = Tools.SQLSetup.SetupSqlConn();
+            //GetWeather();
+            //GetGarMon();
+            //GetCalendar();
+        }
+
+        public void TimerTick(object sender, object e)
+        {
+            txbClock.Text = $"{DateTime.Now.ToString("h:mm:ss tt")}";
+            txbDate.Text = $"{DateTime.Now.DayOfWeek}, {DateTime.Now.Month}/{DateTime.Now.Day}/{DateTime.Now.Year}";
+
+            ticks++;
+            firstCheck++;
+            secondCheck++;
+
+            if(firstCheck >= firstInt)
+            {
+                sqlOnline = Tools.SQLSetup.SetupSqlConn();
+                //GetGarmon();
+                firstCheck = 0;
+            }
+
+            if(secondCheck >= secondInt)
+            {
+                //GetWeather();
+                //GetCalendar();
+                secondCheck = 0;
+            }
+        }
+
+        public void GetGarmon()
+        {
+
+        }
+
+        public void GetWeather()
+        {
+
+        }
+
+        public void GetCalendar()
+        {
+
+        }
+        
+
+    }
+}
---
+++ InfoSim.UI/Tools/SQLSetup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace InfoSim.UI.Tools
+{
+    class SQLSetup
+    {
+        //private static string sqlConn = @"Server=tcp:WAMPA,49172\SQLEXPRESS;Database=BarringtonDB;Trusted_Connection=True;User Id=albinodyno;Password=thelivingshitouttame";
+
+        public static bool SetupSqlConn()
+        {
+            try
+            {
+                string sqlConn = @"Server=tcp:WAMPA,49172\SQLEXPRESS;Database=BarringtonDB;Trusted_Connection=False;User Id=albinodyno;Password=thelivingshitouttame";
+
+                SqlConnection connection = new SqlConnection(sqlConn);
+                SqlCommand cmd = new SqlCommand("AppTestConnection", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@App", "InfoSim");
+
+                connection.Open();
+                int i = cmd.ExecuteNonQuery();
+                connection.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+    }
+}
